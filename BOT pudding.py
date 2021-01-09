0b3ca8d1f2351f7a6773a22c0f15877beb4442b9
pudding233 <2480751781@qq.com>
2021-01-09 19:21:15 +0800

    成功添加好感值

--- BOT pudding.py
+++ BOT pudding.py
@@ -8,19 +8,29 @@ intchushi = int(chushi) #初始值int转换
 if intchushi == 1 or intchushi == 2: #判断值是不是1或者2，如果是1或者2返回True
     if intchushi == 1: #判断值是不是1，如果是1返回Ture
         caice = input("qwq猜猜我想的是什么数字吧?")
-        suiji = random.randint(1,10) #一个从1到10的随机数字（包括1和10）
+        suiji = random.randint(1,2) #一个从1到10的随机数字（包括1和10）
         while caice !=suiji:
             caice = input("再试一试？猜猜布丁想的是什么数字")
             caice = int(caice)
             if caice == suiji:
                 print ("QAQ你竟然知道")
+                duqu2 = open("ceshi.txt")
+                duqu3 = duqu2.read()
+                duqu4 = int(duqu3)
+                duqu2.close()
+                cucun1 = duqu4 + 1
+                cucun2 = str(cucun1)
+                cucun = open("ceshi.txt", "w")
+                cucun.write(cucun2)
+                print ("布丁对你的好感度+1")
             else:
                 if caice > suiji:
                         print ("好像大了")
                 else:
                         print ("好像小了")
     else:
-        print ("QWQ读取储存数值还不会") #未完成
+        duqu = open("ceshi.txt")
+        print("好感度为:"+duqu.read())
 else: #初始输入的值不是1或者2就运行这里
     if intchushi == 3: #如果是3就退出程序
         print ("程序退出")
